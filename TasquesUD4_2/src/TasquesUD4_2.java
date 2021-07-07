6abf35bf00ae293b42a57751aa38f3593d0c79cc
joelGit99 <fernandez.rodriguez.joel.21@gmail.com>
2021-07-07 11:07:39 +0200

    Tarea 2 de la Unidad 4

---
+++ TasquesUD4_2/src/TasquesUD4_2.java
@@ -0,0 +1,22 @@
+
+public class TasquesUD4_2 {
+	/**
+	 * Programa que realiza diferentes operaciones
+	 * @author Joel
+	 */
+	public static void main(String[] args) {
+		int n = 8;
+		double a = 9.5;
+		char c = 'b';
+		double suma = n + a;
+		double resta = a - n;
+		
+		System.out.println("Variable N = " + n);
+		System.out.println("Variable A = " + a);
+		System.out.println("Variable C = " + c);
+		System.out.println(n + " + " + a + " = " + suma);
+		System.out.println(a + " + " + n + " = " + resta);
+		System.out.println("El valor numérico del carácter " + c + " = " + (int)c);
+	}
+
+}
